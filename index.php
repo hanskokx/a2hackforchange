00ff7c4f5657f067c2efe355d9a4e036c4721aa9
Hans Kokx <skipmeister123@gmail.com>
2013-03-24 00:06:53 -0400

    Updated header

--- index.php
+++ index.php
@@ -26,12 +26,13 @@
 >>>>>>> 4dc209e77f46f2bfe709b2d0eeeafba27596cc3a
 <div id="header" class="shadow">
 	<div class="nav">
-		<a href="#what"><span id="what_btn" class="nav_button">What</span></a>
-		<a href="#why"><span id="why_btn" class="nav_button">Why</span></a>
-		<a href="#who"><span id="who_btn" class="nav_button">Who</span></a>
-		<a href="#when"><span id="when_btn" class="nav_button">When</span></a>
-		<a href="#where"><span id="where_btn" class="nav_button">Where</span></a>
+		<div id="left-grad"></div>
+		<a href="#what"><span id="about_btn" class="nav_button">About</span></a>
+		<a href="#event"><span id="event_btn" class="nav_button">Event</span></a>
+		<span class="nav_div">&nbsp</span>
+		<a href="#get_involved"><span id="get_involved_btn" class="nav_button">Help Out</span></a>
 		<a href="#faq"><span id="faq_btn" class="nav_button">FAQ</span></a>
+		<div id="right-grad"></div>
 	</div>
 </div>
 
@@ -40,11 +41,13 @@
 	<p style="line-height: 50px">&nbsp;</p>
 	<h1>What</h1>
 		<span class="text">
-Ann Arbor's very own Day of Civic Hacking is part of a nationwide event that will take place June 1-2, 2013.
+National Day of Civic Hacking is a national event that will take place June 1-2, 2013, in cities across the nation. The event will bring together citizens, software developers, and entrepreneurs from all over the nation to collaboratively create, build, and invent new solutions using publicly-released data, code and technology to solve challenges relevant to our neighborhoods, our cities, our states and our country. National Day of Civic Hacking will provide citizens an opportunity to do what is most quintessentially American: roll up our sleeves, get involved and work together to improve our society.
+
+The event will leverage the expertise and entrepreneurial spirit of those outside federal, state and local government to drive meaningful, technology-based solutions for federal, state and local government. It demonstrates what's possible when we all work together to strengthen our society and our lives. YOU can make a difference no matter where you live. 
 		</span>
 
 	<div class="divider"></div>
-	<a name="why"></a>
+	<a name="event"></a>
 	<p style="line-height: 50px">&nbsp;</p>
 	<h1>Why</h1>
 		<span class="text">
@@ -62,7 +65,7 @@ Nunc sodales tempus tellus ac viverra. Vestibulum vel mauris elit. Cras est turp
 		</span>
 
 	<div class="divider"></div>
-	<a name="who"></a>
+	<a name="get_involved"></a>
 	<p style="line-height: 50px">&nbsp;</p>
 	<h1>Who</h1>
 		<span class="text">
@@ -79,41 +82,6 @@ Etiam at justo id magna egestas semper. Duis eleifend elementum velit cursus por
 Nunc sodales tempus tellus ac viverra. Vestibulum vel mauris elit. Cras est turpis, adipiscing a vulputate viverra, tristique ut mi. Fusce fringilla neque nisl, at ultrices nunc. Proin tincidunt rhoncus pulvinar. In eu dictum est. Quisque nulla dolor, commodo nec facilisis vitae, elementum nec dolor. Maecenas malesuada iaculis velit, id dapibus dui dignissim ac. Mauris quis tempus ante. Maecenas ligula enim, aliquam ut feugiat ut, mollis ac elit. Nam quis neque eu purus elementum pharetra ac eget ipsum. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. 
 		</span>
 
-	<div class="divider"></div>
-	<a name="when"></a>
-	<p style="line-height: 50px">&nbsp;</p>
-	<h1>When</h1>
-		<span class="text">
-
-
-Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed egestas posuere sodales. Vivamus porta risus vel felis tincidunt a aliquet ipsum rutrum. Suspendisse auctor purus mauris. Duis eu dui imperdiet metus accumsan lobortis vitae sit amet justo. Integer interdum varius eros, et malesuada urna interdum at. Nullam placerat, sapien id convallis dictum, neque sapien tempus augue, vitae imperdiet quam risus et ante. Vestibulum pulvinar viverra mi, ac viverra lacus commodo sed. Vivamus ac enim et dui ultrices sodales. Proin malesuada fringilla urna, at placerat arcu faucibus a. Sed et est ut est ultrices congue non consequat mauris. Vestibulum congue vulputate quam vitae vehicula. Fusce blandit mollis mollis. Suspendisse sed odio in nunc tincidunt commodo nec quis ligula. Sed elit quam, imperdiet ac suscipit quis, blandit quis lacus. Duis nec feugiat ligula.
-
-Suspendisse porttitor pellentesque tincidunt. Aenean blandit lacinia nulla, eu rutrum orci blandit quis. Sed nibh diam, auctor vel pulvinar a, cursus eget est. Pellentesque vestibulum elementum magna, eu euismod dui imperdiet vel. Aliquam posuere augue eu turpis imperdiet volutpat eleifend quam blandit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum pretium ante aliquet nisi ullamcorper id mollis turpis adipiscing. Nullam vestibulum dui nulla.
-
-Vivamus vulputate nibh sit amet ipsum egestas sodales. Donec id metus lectus, sit amet ornare ipsum. Sed dignissim massa sed magna blandit eget faucibus dui pellentesque. Phasellus et sapien ut purus auctor accumsan sed non felis. Cras non magna ante, consequat laoreet metus. Nullam magna augue, dignissim non adipiscing ac, condimentum ac est. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae;
-
-Etiam at justo id magna egestas semper. Duis eleifend elementum velit cursus porttitor. Sed ac risus in odio elementum placerat. Maecenas interdum massa eu urna placerat condimentum. Cras rutrum gravida pellentesque. Aliquam vitae mi arcu. Ut a lacus eros. Aenean arcu risus, suscipit et laoreet a, vehicula vitae est.
-
-Nunc sodales tempus tellus ac viverra. Vestibulum vel mauris elit. Cras est turpis, adipiscing a vulputate viverra, tristique ut mi. Fusce fringilla neque nisl, at ultrices nunc. Proin tincidunt rhoncus pulvinar. In eu dictum est. Quisque nulla dolor, commodo nec facilisis vitae, elementum nec dolor. Maecenas malesuada iaculis velit, id dapibus dui dignissim ac. Mauris quis tempus ante. Maecenas ligula enim, aliquam ut feugiat ut, mollis ac elit. Nam quis neque eu purus elementum pharetra ac eget ipsum. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. 
-		</span>
-
-	<div class="divider"></div>
-	<a name="where"></a>
-	<p style="line-height: 50px">&nbsp;</p>
-	<h1>Where</h1>
-		<span class="text">
-
-
-Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed egestas posuere sodales. Vivamus porta risus vel felis tincidunt a aliquet ipsum rutrum. Suspendisse auctor purus mauris. Duis eu dui imperdiet metus accumsan lobortis vitae sit amet justo. Integer interdum varius eros, et malesuada urna interdum at. Nullam placerat, sapien id convallis dictum, neque sapien tempus augue, vitae imperdiet quam risus et ante. Vestibulum pulvinar viverra mi, ac viverra lacus commodo sed. Vivamus ac enim et dui ultrices sodales. Proin malesuada fringilla urna, at placerat arcu faucibus a. Sed et est ut est ultrices congue non consequat mauris. Vestibulum congue vulputate quam vitae vehicula. Fusce blandit mollis mollis. Suspendisse sed odio in nunc tincidunt commodo nec quis ligula. Sed elit quam, imperdiet ac suscipit quis, blandit quis lacus. Duis nec feugiat ligula.
-
-Suspendisse porttitor pellentesque tincidunt. Aenean blandit lacinia nulla, eu rutrum orci blandit quis. Sed nibh diam, auctor vel pulvinar a, cursus eget est. Pellentesque vestibulum elementum magna, eu euismod dui imperdiet vel. Aliquam posuere augue eu turpis imperdiet volutpat eleifend quam blandit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum pretium ante aliquet nisi ullamcorper id mollis turpis adipiscing. Nullam vestibulum dui nulla.
-
-Vivamus vulputate nibh sit amet ipsum egestas sodales. Donec id metus lectus, sit amet ornare ipsum. Sed dignissim massa sed magna blandit eget faucibus dui pellentesque. Phasellus et sapien ut purus auctor accumsan sed non felis. Cras non magna ante, consequat laoreet metus. Nullam magna augue, dignissim non adipiscing ac, condimentum ac est. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae;
-
-Etiam at justo id magna egestas semper. Duis eleifend elementum velit cursus porttitor. Sed ac risus in odio elementum placerat. Maecenas interdum massa eu urna placerat condimentum. Cras rutrum gravida pellentesque. Aliquam vitae mi arcu. Ut a lacus eros. Aenean arcu risus, suscipit et laoreet a, vehicula vitae est.
-
-Nunc sodales tempus tellus ac viverra. Vestibulum vel mauris elit. Cras est turpis, adipiscing a vulputate viverra, tristique ut mi. Fusce fringilla neque nisl, at ultrices nunc. Proin tincidunt rhoncus pulvinar. In eu dictum est. Quisque nulla dolor, commodo nec facilisis vitae, elementum nec dolor. Maecenas malesuada iaculis velit, id dapibus dui dignissim ac. Mauris quis tempus ante. Maecenas ligula enim, aliquam ut feugiat ut, mollis ac elit. Nam quis neque eu purus elementum pharetra ac eget ipsum. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. 
-		</span>
 
 	<div class="divider"></div>
 	<a name="faq"></a>
