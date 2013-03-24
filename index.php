4dc209e77f46f2bfe709b2d0eeeafba27596cc3a
Hans Kokx <skipmeister123@gmail.com>
2013-03-23 23:08:50 -0400

    Initial Commit

---
+++ index.php
@@ -0,0 +1,142 @@
+
+
+<!DOCTYPE html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+		<!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+			<!--[if lt IE 9]>
+				<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+			<![endif]
+
+		<!-- Stylesheets -->
+		<link rel="stylesheet" href="./assets/css/styles.css" media="all" type="text/css" />
+
+		<!-- Fonts -->
+		<link href='http://fonts.googleapis.com/css?family=Alfa+Slab+One' rel='stylesheet' type='text/css'>
+		<link href='http://fonts.googleapis.com/css?family=Open+Sans+Condensed:700,300italic,300' rel='stylesheet' type='text/css'>
+		<link href='http://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
+
+	</head>
+<body>
+
+<div id="blast"></div>
+<div id="header" class="shadow">
+	<div class="nav">
+		<a href="#what"><span id="what_btn" class="nav_button">What</span></a>
+		<a href="#why"><span id="why_btn" class="nav_button">Why</span></a>
+		<a href="#who"><span id="who_btn" class="nav_button">Who</span></a>
+		<a href="#when"><span id="when_btn" class="nav_button">When</span></a>
+		<a href="#where"><span id="where_btn" class="nav_button">Where</span></a>
+		<a href="#faq"><span id="faq_btn" class="nav_button">FAQ</span></a>
+	</div>
+</div>
+
+<div id="content">
+	<a name="what"></a>
+	<p style="line-height: 50px">&nbsp;</p>
+	<h1>What</h1>
+		<span class="text">
+Ann Arbor's very own Day of Civic Hacking is part of a nationwide event that will take place June 1-2, 2013.
+		</span>
+
+	<div class="divider"></div>
+	<a name="why"></a>
+	<p style="line-height: 50px">&nbsp;</p>
+	<h1>Why</h1>
+		<span class="text">
+
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed egestas posuere sodales. Vivamus porta risus vel felis tincidunt a aliquet ipsum rutrum. Suspendisse auctor purus mauris. Duis eu dui imperdiet metus accumsan lobortis vitae sit amet justo. Integer interdum varius eros, et malesuada urna interdum at. Nullam placerat, sapien id convallis dictum, neque sapien tempus augue, vitae imperdiet quam risus et ante. Vestibulum pulvinar viverra mi, ac viverra lacus commodo sed. Vivamus ac enim et dui ultrices sodales. Proin malesuada fringilla urna, at placerat arcu faucibus a. Sed et est ut est ultrices congue non consequat mauris. Vestibulum congue vulputate quam vitae vehicula. Fusce blandit mollis mollis. Suspendisse sed odio in nunc tincidunt commodo nec quis ligula. Sed elit quam, imperdiet ac suscipit quis, blandit quis lacus. Duis nec feugiat ligula.
+
+Suspendisse porttitor pellentesque tincidunt. Aenean blandit lacinia nulla, eu rutrum orci blandit quis. Sed nibh diam, auctor vel pulvinar a, cursus eget est. Pellentesque vestibulum elementum magna, eu euismod dui imperdiet vel. Aliquam posuere augue eu turpis imperdiet volutpat eleifend quam blandit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum pretium ante aliquet nisi ullamcorper id mollis turpis adipiscing. Nullam vestibulum dui nulla.
+
+Vivamus vulputate nibh sit amet ipsum egestas sodales. Donec id metus lectus, sit amet ornare ipsum. Sed dignissim massa sed magna blandit eget faucibus dui pellentesque. Phasellus et sapien ut purus auctor accumsan sed non felis. Cras non magna ante, consequat laoreet metus. Nullam magna augue, dignissim non adipiscing ac, condimentum ac est. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae;
+
+Etiam at justo id magna egestas semper. Duis eleifend elementum velit cursus porttitor. Sed ac risus in odio elementum placerat. Maecenas interdum massa eu urna placerat condimentum. Cras rutrum gravida pellentesque. Aliquam vitae mi arcu. Ut a lacus eros. Aenean arcu risus, suscipit et laoreet a, vehicula vitae est.
+
+Nunc sodales tempus tellus ac viverra. Vestibulum vel mauris elit. Cras est turpis, adipiscing a vulputate viverra, tristique ut mi. Fusce fringilla neque nisl, at ultrices nunc. Proin tincidunt rhoncus pulvinar. In eu dictum est. Quisque nulla dolor, commodo nec facilisis vitae, elementum nec dolor. Maecenas malesuada iaculis velit, id dapibus dui dignissim ac. Mauris quis tempus ante. Maecenas ligula enim, aliquam ut feugiat ut, mollis ac elit. Nam quis neque eu purus elementum pharetra ac eget ipsum. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. 
+		</span>
+
+	<div class="divider"></div>
+	<a name="who"></a>
+	<p style="line-height: 50px">&nbsp;</p>
+	<h1>Who</h1>
+		<span class="text">
+
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed egestas posuere sodales. Vivamus porta risus vel felis tincidunt a aliquet ipsum rutrum. Suspendisse auctor purus mauris. Duis eu dui imperdiet metus accumsan lobortis vitae sit amet justo. Integer interdum varius eros, et malesuada urna interdum at. Nullam placerat, sapien id convallis dictum, neque sapien tempus augue, vitae imperdiet quam risus et ante. Vestibulum pulvinar viverra mi, ac viverra lacus commodo sed. Vivamus ac enim et dui ultrices sodales. Proin malesuada fringilla urna, at placerat arcu faucibus a. Sed et est ut est ultrices congue non consequat mauris. Vestibulum congue vulputate quam vitae vehicula. Fusce blandit mollis mollis. Suspendisse sed odio in nunc tincidunt commodo nec quis ligula. Sed elit quam, imperdiet ac suscipit quis, blandit quis lacus. Duis nec feugiat ligula.
+
+Suspendisse porttitor pellentesque tincidunt. Aenean blandit lacinia nulla, eu rutrum orci blandit quis. Sed nibh diam, auctor vel pulvinar a, cursus eget est. Pellentesque vestibulum elementum magna, eu euismod dui imperdiet vel. Aliquam posuere augue eu turpis imperdiet volutpat eleifend quam blandit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum pretium ante aliquet nisi ullamcorper id mollis turpis adipiscing. Nullam vestibulum dui nulla.
+
+Vivamus vulputate nibh sit amet ipsum egestas sodales. Donec id metus lectus, sit amet ornare ipsum. Sed dignissim massa sed magna blandit eget faucibus dui pellentesque. Phasellus et sapien ut purus auctor accumsan sed non felis. Cras non magna ante, consequat laoreet metus. Nullam magna augue, dignissim non adipiscing ac, condimentum ac est. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae;
+
+Etiam at justo id magna egestas semper. Duis eleifend elementum velit cursus porttitor. Sed ac risus in odio elementum placerat. Maecenas interdum massa eu urna placerat condimentum. Cras rutrum gravida pellentesque. Aliquam vitae mi arcu. Ut a lacus eros. Aenean arcu risus, suscipit et laoreet a, vehicula vitae est.
+
+Nunc sodales tempus tellus ac viverra. Vestibulum vel mauris elit. Cras est turpis, adipiscing a vulputate viverra, tristique ut mi. Fusce fringilla neque nisl, at ultrices nunc. Proin tincidunt rhoncus pulvinar. In eu dictum est. Quisque nulla dolor, commodo nec facilisis vitae, elementum nec dolor. Maecenas malesuada iaculis velit, id dapibus dui dignissim ac. Mauris quis tempus ante. Maecenas ligula enim, aliquam ut feugiat ut, mollis ac elit. Nam quis neque eu purus elementum pharetra ac eget ipsum. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. 
+		</span>
+
+	<div class="divider"></div>
+	<a name="when"></a>
+	<p style="line-height: 50px">&nbsp;</p>
+	<h1>When</h1>
+		<span class="text">
+
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed egestas posuere sodales. Vivamus porta risus vel felis tincidunt a aliquet ipsum rutrum. Suspendisse auctor purus mauris. Duis eu dui imperdiet metus accumsan lobortis vitae sit amet justo. Integer interdum varius eros, et malesuada urna interdum at. Nullam placerat, sapien id convallis dictum, neque sapien tempus augue, vitae imperdiet quam risus et ante. Vestibulum pulvinar viverra mi, ac viverra lacus commodo sed. Vivamus ac enim et dui ultrices sodales. Proin malesuada fringilla urna, at placerat arcu faucibus a. Sed et est ut est ultrices congue non consequat mauris. Vestibulum congue vulputate quam vitae vehicula. Fusce blandit mollis mollis. Suspendisse sed odio in nunc tincidunt commodo nec quis ligula. Sed elit quam, imperdiet ac suscipit quis, blandit quis lacus. Duis nec feugiat ligula.
+
+Suspendisse porttitor pellentesque tincidunt. Aenean blandit lacinia nulla, eu rutrum orci blandit quis. Sed nibh diam, auctor vel pulvinar a, cursus eget est. Pellentesque vestibulum elementum magna, eu euismod dui imperdiet vel. Aliquam posuere augue eu turpis imperdiet volutpat eleifend quam blandit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum pretium ante aliquet nisi ullamcorper id mollis turpis adipiscing. Nullam vestibulum dui nulla.
+
+Vivamus vulputate nibh sit amet ipsum egestas sodales. Donec id metus lectus, sit amet ornare ipsum. Sed dignissim massa sed magna blandit eget faucibus dui pellentesque. Phasellus et sapien ut purus auctor accumsan sed non felis. Cras non magna ante, consequat laoreet metus. Nullam magna augue, dignissim non adipiscing ac, condimentum ac est. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae;
+
+Etiam at justo id magna egestas semper. Duis eleifend elementum velit cursus porttitor. Sed ac risus in odio elementum placerat. Maecenas interdum massa eu urna placerat condimentum. Cras rutrum gravida pellentesque. Aliquam vitae mi arcu. Ut a lacus eros. Aenean arcu risus, suscipit et laoreet a, vehicula vitae est.
+
+Nunc sodales tempus tellus ac viverra. Vestibulum vel mauris elit. Cras est turpis, adipiscing a vulputate viverra, tristique ut mi. Fusce fringilla neque nisl, at ultrices nunc. Proin tincidunt rhoncus pulvinar. In eu dictum est. Quisque nulla dolor, commodo nec facilisis vitae, elementum nec dolor. Maecenas malesuada iaculis velit, id dapibus dui dignissim ac. Mauris quis tempus ante. Maecenas ligula enim, aliquam ut feugiat ut, mollis ac elit. Nam quis neque eu purus elementum pharetra ac eget ipsum. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. 
+		</span>
+
+	<div class="divider"></div>
+	<a name="where"></a>
+	<p style="line-height: 50px">&nbsp;</p>
+	<h1>Where</h1>
+		<span class="text">
+
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed egestas posuere sodales. Vivamus porta risus vel felis tincidunt a aliquet ipsum rutrum. Suspendisse auctor purus mauris. Duis eu dui imperdiet metus accumsan lobortis vitae sit amet justo. Integer interdum varius eros, et malesuada urna interdum at. Nullam placerat, sapien id convallis dictum, neque sapien tempus augue, vitae imperdiet quam risus et ante. Vestibulum pulvinar viverra mi, ac viverra lacus commodo sed. Vivamus ac enim et dui ultrices sodales. Proin malesuada fringilla urna, at placerat arcu faucibus a. Sed et est ut est ultrices congue non consequat mauris. Vestibulum congue vulputate quam vitae vehicula. Fusce blandit mollis mollis. Suspendisse sed odio in nunc tincidunt commodo nec quis ligula. Sed elit quam, imperdiet ac suscipit quis, blandit quis lacus. Duis nec feugiat ligula.
+
+Suspendisse porttitor pellentesque tincidunt. Aenean blandit lacinia nulla, eu rutrum orci blandit quis. Sed nibh diam, auctor vel pulvinar a, cursus eget est. Pellentesque vestibulum elementum magna, eu euismod dui imperdiet vel. Aliquam posuere augue eu turpis imperdiet volutpat eleifend quam blandit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum pretium ante aliquet nisi ullamcorper id mollis turpis adipiscing. Nullam vestibulum dui nulla.
+
+Vivamus vulputate nibh sit amet ipsum egestas sodales. Donec id metus lectus, sit amet ornare ipsum. Sed dignissim massa sed magna blandit eget faucibus dui pellentesque. Phasellus et sapien ut purus auctor accumsan sed non felis. Cras non magna ante, consequat laoreet metus. Nullam magna augue, dignissim non adipiscing ac, condimentum ac est. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae;
+
+Etiam at justo id magna egestas semper. Duis eleifend elementum velit cursus porttitor. Sed ac risus in odio elementum placerat. Maecenas interdum massa eu urna placerat condimentum. Cras rutrum gravida pellentesque. Aliquam vitae mi arcu. Ut a lacus eros. Aenean arcu risus, suscipit et laoreet a, vehicula vitae est.
+
+Nunc sodales tempus tellus ac viverra. Vestibulum vel mauris elit. Cras est turpis, adipiscing a vulputate viverra, tristique ut mi. Fusce fringilla neque nisl, at ultrices nunc. Proin tincidunt rhoncus pulvinar. In eu dictum est. Quisque nulla dolor, commodo nec facilisis vitae, elementum nec dolor. Maecenas malesuada iaculis velit, id dapibus dui dignissim ac. Mauris quis tempus ante. Maecenas ligula enim, aliquam ut feugiat ut, mollis ac elit. Nam quis neque eu purus elementum pharetra ac eget ipsum. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. 
+		</span>
+
+	<div class="divider"></div>
+	<a name="faq"></a>
+	<p style="line-height: 50px">&nbsp;</p>
+	<h1>FAQ</h1>
+		<span class="text">
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed egestas posuere sodales. Vivamus porta risus vel felis tincidunt a aliquet ipsum rutrum. Suspendisse auctor purus mauris. Duis eu dui imperdiet metus accumsan lobortis vitae sit amet justo. Integer interdum varius eros, et malesuada urna interdum at. Nullam placerat, sapien id convallis dictum, neque sapien tempus augue, vitae imperdiet quam risus et ante. Vestibulum pulvinar viverra mi, ac viverra lacus commodo sed. Vivamus ac enim et dui ultrices sodales. Proin malesuada fringilla urna, at placerat arcu faucibus a. Sed et est ut est ultrices congue non consequat mauris. Vestibulum congue vulputate quam vitae vehicula. Fusce blandit mollis mollis. Suspendisse sed odio in nunc tincidunt commodo nec quis ligula. Sed elit quam, imperdiet ac suscipit quis, blandit quis lacus. Duis nec feugiat ligula.
+
+Suspendisse porttitor pellentesque tincidunt. Aenean blandit lacinia nulla, eu rutrum orci blandit quis. Sed nibh diam, auctor vel pulvinar a, cursus eget est. Pellentesque vestibulum elementum magna, eu euismod dui imperdiet vel. Aliquam posuere augue eu turpis imperdiet volutpat eleifend quam blandit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum pretium ante aliquet nisi ullamcorper id mollis turpis adipiscing. Nullam vestibulum dui nulla.
+
+Vivamus vulputate nibh sit amet ipsum egestas sodales. Donec id metus lectus, sit amet ornare ipsum. Sed dignissim massa sed magna blandit eget faucibus dui pellentesque. Phasellus et sapien ut purus auctor accumsan sed non felis. Cras non magna ante, consequat laoreet metus. Nullam magna augue, dignissim non adipiscing ac, condimentum ac est. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae;
+
+Etiam at justo id magna egestas semper. Duis eleifend elementum velit cursus porttitor. Sed ac risus in odio elementum placerat. Maecenas interdum massa eu urna placerat condimentum. Cras rutrum gravida pellentesque. Aliquam vitae mi arcu. Ut a lacus eros. Aenean arcu risus, suscipit et laoreet a, vehicula vitae est.
+
+Nunc sodales tempus tellus ac viverra. Vestibulum vel mauris elit. Cras est turpis, adipiscing a vulputate viverra, tristique ut mi. Fusce fringilla neque nisl, at ultrices nunc. Proin tincidunt rhoncus pulvinar. In eu dictum est. Quisque nulla dolor, commodo nec facilisis vitae, elementum nec dolor. Maecenas malesuada iaculis velit, id dapibus dui dignissim ac. Mauris quis tempus ante. Maecenas ligula enim, aliquam ut feugiat ut, mollis ac elit. Nam quis neque eu purus elementum pharetra ac eget ipsum. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. 
+		</span>
+
+<p style="line-height: 35px">&nbsp;</p>
+</div>
+
+<div id="footer">
+	<a href="mailto:&#104;&#101;&#108;&#108;&#111;&#064;&#097;&#050;&#104;&#097;&#099;&#107;&#102;&#111;&#114;&#099;&#104;&#097;&#110;&#103;&#101;&#046;&#111;&#114;&#103;?subject=A2%20Hack%20for%20Change"><span id="contact"></span></a>
+</div>
+
+<!-- Javascript last, so the page loads supa-quick -->
+	<script type='text/javascript' src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+
+</body>
+</html>r
