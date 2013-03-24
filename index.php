83f4fd735c8f1f4dd515cede7e2040dac9927806
Hans Kokx <skipmeister123@gmail.com>
2013-03-24 17:40:53 -0400

    Added Woopra analytics

--- index.php
+++ index.php
@@ -67,6 +67,31 @@
 			
 		});
 		</script>
+
+		<!-- Start of Woopra Code -->
+			<script type="text/javascript">
+			function woopraReady(tracker) {
+			    tracker.setDomain('a2hackforchange.org');
+			    tracker.setIdleTimeout(300000);
+
+			    // Make sure you add the visitor properties before the track() function.
+			    tracker.addVisitorProperty('name', '$account.name');
+			    tracker.addVisitorProperty('email', '$account.email');
+			    tracker.addVisitorProperty('company', '$account.company');
+
+			    tracker.trackPageview({type:'pageview',url:window.location.pathname+window.location.search,title:document.title});
+			    return false;
+			}
+			(function() {
+			    var wsc = document.createElement('script');
+			    wsc.src = document.location.protocol+'//static.woopra.com/js/woopra.js';
+			    wsc.type = 'text/javascript';
+			    wsc.async = true;
+			    var ssc = document.getElementsByTagName('script')[0];
+			    ssc.parentNode.insertBefore(wsc, ssc);
+			})();
+			</script>
+		<!-- End of Woopra Code -->
 	</head>
 <body>
 
