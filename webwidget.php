5f6b1a8e64746bf28f87e519000a4697304c316b
Jason Hejna <jason.hejna@gmail.com>
2013-03-24 23:32:11 -0400

    webwidget for testing

--- webwidget.php
+++ webwidget.php
@@ -3,13 +3,27 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-        <link rel="stylesheet" href="assets/css/style.css" />
+        <link rel="stylesheet" href="assets/css/widget.css" />
         <!-- jQuery -->
 		<link href="assets/js/jquery/css/ui-lightness/jquery-ui-1.10.2.custom.css" rel="stylesheet">
 		<script src="assets/js/jquery/js/jquery-1.9.1.js"></script>
 		<script src="assets/js/jquery/js/jquery-ui-1.10.2.custom.js"></script>
+		<script type="text/javascript">
+		$(document).ready(function(){
+			$('#mapimage').hide();
+		$('#front').click(function() {
+			
+        	$("#logo").toggle();
+        	$("#mapimage").toggle();
+        });
+		});
+		</script>
 	</head>
 	<body>
-		hello
+		<div id="front">
+			<div id="logo"></div>
+			<div id="mapimage"></div>
+		</div>
+		
 	</body>
 </html>
