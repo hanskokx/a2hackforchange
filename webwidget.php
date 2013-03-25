1e4e04d43553e9a629d088c688c7437add02bfea
Jason Hejna <jason.hejna@gmail.com>
2013-03-25 13:39:55 -0400

    third party widget

--- webwidget.php
+++ webwidget.php
@@ -4,6 +4,8 @@
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
         <link rel="stylesheet" href="assets/css/widget.css" />
+        <link href='http://fonts.googleapis.com/css?family=Keania+One|Roboto+Condensed' rel='stylesheet' type='text/css'>
+
         <!-- jQuery -->
 		<link href="assets/js/jquery/css/ui-lightness/jquery-ui-1.10.2.custom.css" rel="stylesheet">
 		<script src="assets/js/jquery/js/jquery-1.9.1.js"></script>
@@ -22,11 +24,12 @@
 		<div id="front">
 			<div id="side1">
 				<div id="logo"></div>
-				<div id="subtitle"></div>
 				
+				<a href="http://a2hackforchange.org" target="_blank" class="classname">Ann Arbor</a>
 				<div id="abouttext"><h2>June 1-2, 2013</h2>The event will leverage the expertise and entrepreneurial spirit of those outside federal, state and local government to drive meaningful, technology-based solutions for federal, state and local government. It demonstrates what's possible when we all work together to strengthen our society and our lives. YOU can make a difference no matter where you live. </div>
 			</div>
 			<div id="side2">
+				<div id="logo"></div>
 				<div id="mapimage"></div>
 			</div>
 		</div>
--- widgetcreator.php
+++ widgetcreator.php
@@ -3,11 +3,12 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-        <link rel="stylesheet" href="assets/css/widget.css" />
+        <link rel="stylesheet" href="assets/css/widgetcreator.css" />
         <!-- jQuery -->
 		<link href="assets/js/jquery/css/ui-lightness/jquery-ui-1.10.2.custom.css" rel="stylesheet">
 		<script src="assets/js/jquery/js/jquery-1.9.1.js"></script>
 		<script src="assets/js/jquery/js/jquery-ui-1.10.2.custom.js"></script>
+		<link href='http://fonts.googleapis.com/css?family=Keania+One|Roboto+Condensed' rel='stylesheet' type='text/css'>
 		<script type-"text/javascript">
 		$(document).ready(function(){
 
@@ -23,13 +24,20 @@
 		</script>
 	</head>
 	<body>
-	Background-Color hex:<input type="text" id="color" value="#FFF" size="8" maxlength="12" onfocus="(this.value = '#')">
-	<br>Width:<input type="text" id="width" value="275" size="7" maxlength="7" onfocus="(this.value = '')">
-	<br>Height:<input type="text" id="height" value="405" size="7" maxlength="7" onfocus="(this.value = '')">
-	<br><button id="asubmit">Calculate</button>
-
-	<div id="outsideiframe">
-		<iframe name='iframe' id="iframe" src="http://a2hackforchange.org/webwidget.php" scrolling="no"  frameborder="0" border="0" cellspacing="0" style="border-style: none; width: 275px; height: 400px; background-color: #FFF; "></iframe>
+	<div id="centercontent">
+		<img src="assets/images/logo_small.png" alt="national day of social hacking" height="60px" width="260px">
+		<h2>Site Plugin Creator</h2>
+		<div id="acontrolls">
+		
+			Color Hex<input type="text" id="color" name="color" value="#FFF" size="7" maxlength="7" onfocus="(this.value = '#')">
+			<br/><br/>Width:<input type="text" id="width" name="width" value="275" size="7" maxlength="7" onfocus="(this.value = '')">
+			<br/><br/>Height:<input type="text" id="height" name="height" value="405" size="7" maxlength="7" onfocus="(this.value = '')">
+			<br/><br/><button id="asubmit">Calculate</button>
+		</div>
+	
+		<div id="outsideiframe">
+			<iframe name='iframe' id="iframe" src="http://a2hackforchange.org/webwidget.php" scrolling="no"  frameborder="0" border="0" cellspacing="0" style="border-style: none; width: 275px; height: 400px; background-color: #FFF; "></iframe>
+		</div>
 	</div>
 	</body>
 </html>
