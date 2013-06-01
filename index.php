4ad5bf9136209020c2e78100816019fa02b97884
Hans Kokx <skipmeister123@gmail.com>
2013-05-31 22:15:12 -0400

    no more registrations

--- index.php
+++ index.php
@@ -201,8 +201,8 @@
 			<br /><br />
 			We need volunteers from all backgrounds to help make this event a success.  Whether you would like to volunteer your skills, offer sponsorship, or help organize the event, we need you!
 			<h2>Why not sign up?</h2>
-            <!-- Pre-registrations closed. See you at the event! -->
-			<iframe src="https://docs.google.com/forms/d/1R-zh6eL77QQJSCHHGWklFFkFq0b8Xs85zEejOUBWtz8/viewform?embedded=true" width="800" height="400" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>
+            Pre-registrations closed. See you at the event!
+			<!-- <iframe src="https://docs.google.com/forms/d/1R-zh6eL77QQJSCHHGWklFFkFq0b8Xs85zEejOUBWtz8/viewform?embedded=true" width="800" height="400" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe> -->
 			<h2>Join the conversation</h2>
 			The forums are located at <a href="http://forums.a2hackforchange.org">forums.a2hackforchange.org</a>
 		</span>
